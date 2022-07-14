05fd39ea90a7f72884ef28108daee745ff447b9e
Comironus <msevcuk259@gmail.com>
2022-07-15 00:41:47 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+
+    if (!newItem) {
+      alert("Enter an item.")
+      return;
+    }
+    
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    }
+
+    setItems(oldList => [...oldList, item]);
+    setNewItem("");
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
+
+  return (
+    <div className="App">
+
+      <h1>Todo List App</h1>
+
+      <input
+        type="text"
+        placeholder='Add an item...'
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+      />
+
+      <button onClick={() => addItem()}>Add</button>
+
+      <ul>
+        {items.map(item => {
+          return(
+            <li key={item.id}>{item.value} <button className='delete-button' onClick={() => deleteItem(item.id)}>×</button> </li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
